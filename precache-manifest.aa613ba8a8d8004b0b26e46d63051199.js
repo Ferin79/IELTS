cdf447245ecd811bdd81bb19c720553716977da0
Ferin <ferin@localhost.localdomain>
2020-10-12 22:05:43 +0530

    Updates

---
+++ precache-manifest.aa613ba8a8d8004b0b26e46d63051199.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b886a6935ba1b48e715a27c9bfed7435",
+    "url": "/IELTS/index.html"
+  },
+  {
+    "revision": "c30e3d4ce0295be93e2c",
+    "url": "/IELTS/static/css/2.6b5d40a7.chunk.css"
+  },
+  {
+    "revision": "2dc0aa27878972e2a3ef",
+    "url": "/IELTS/static/css/main.3b952aef.chunk.css"
+  },
+  {
+    "revision": "c30e3d4ce0295be93e2c",
+    "url": "/IELTS/static/js/2.eb6f480b.chunk.js"
+  },
+  {
+    "revision": "6ce1330154a001ca508a3c163f47388a",
+    "url": "/IELTS/static/js/2.eb6f480b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dc0aa27878972e2a3ef",
+    "url": "/IELTS/static/js/main.2ef5a5d9.chunk.js"
+  },
+  {
+    "revision": "5c767c1f854c4d8de837",
+    "url": "/IELTS/static/js/runtime-main.e275631c.js"
+  },
+  {
+    "revision": "5ab012741e717cda3e641ad0da58214f",
+    "url": "/IELTS/static/media/skype_remix_2.5ab01274.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.899ff316b176e900dca31e523cb70e13.js"
+  "/IELTS/precache-manifest.aa613ba8a8d8004b0b26e46d63051199.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/IELTS/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
